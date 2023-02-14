b0be75888b2e9226e5ba65721a0f62a3acd130f1
H A S A N <hasan25042019@gmail.com>
2023-02-14 19:14:06 +0600

    Update index.php
    fgg

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-    echo "hi";
+    $students = [
+        [
+        'name' => 'Hasan',
+        'age' => 22
+        ],
+        [
+        'name' => 'Masud',
+        'age' => 20
+        ],
+        [
+        'name' => 'Ruhani',
+        'age' => 25
+        ],
+        [
+        'name' => 'Abul',
+        'age' => 30
+        ]
+    ];
+
+    function displayStudent($st){
+        // echo $st['name']."-".$st['age']."\n";
+        echo "{$st['name']} - {$st['age']} \n" ;
+    }
+    foreach($students as $student){
+        displayStudent($student);
+        // echo $student['name']."-".$student['age']."\n";
+    }
