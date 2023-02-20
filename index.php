4987b646207755491c23dae3f609d1bb87365ed0
H A S A N <hasan25042019@gmail.com>
2023-02-20 20:28:55 +0600

    Update index.php
    dfd

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
-    echo "hi";
+    function findSecondlargest($arr) {
+        if(empty($arr)) {
+            return;
+        }
+
+        $max = PHP_INT_MIN;
+        $secondMax = PHP_INT_MIN;
+
+        foreach($arr as $number) {
+
+                if($number > $max) {
+                    $secondMax = $max;
+                    $max = $number;
+                }
+
+            if($number > $secondMax && $number < $max) {
+                $secondMax = $number;
+            }
+        }
+        return $secondMax;
+    }
+    $arr = array(100, 40, 12, 15, 185, 90, 79, 69, 55, 30, 21);
+    $second_maximum = findSecondlargest($arr);
+    echo "Second Highest Element is ".$second_maximum;
