7ffd2ca3cde73c60f7b54f65c62a8d7abb5cb521
H A S A N <hasan25042019@gmail.com>
2023-02-27 19:51:36 +0600

    Update index.php
    dffd

--- index.php
+++ index.php
@@ -1,2 +1,101 @@
-<?php
-echo "hi Hasan";
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
+    </head>
+    <style>.col {
+    -ms-flex-preferred-size: 0;
+    flex-basis: 0;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    max-width: 100%;
+    border: solid bisque;
+    padding-bottom: 200px;
+    margin: 45px 7px;
+    padding-top: 30px;
+    background: bisque;
+    border-radius: 10px;
+}
+    h2 {
+    font-size: 2rem;
+    font-family: roboto;
+    font-weight: 600;
+    color: lightslategrey;
+    }
+</style>
+    <body>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+    <form method="POST">
+                        <h2>Task-01</h2>
+                        <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                
+                                    <label for="email" class="input-group-text">Name:</label><br>
+                                </div>
+                                
+                                <input type="text" id="name" name="name" placeholder="Name" required><br>
+                            </div>
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <label for="email" class="input-group-text">Email:</label><br>
+                                </div>
+                                <input type="password" class="form-control" placeholder="E-mail">
+                            </div>
+                            <div class="form-group" text="center">
+						        <input type="submit" value="Submit" class="btn float-right login_btn">
+					        </div>
+                        </form>
+    </div>
+    <div class="col">
+    <form>
+    <h2>Task-01</h2>
+                        <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <label for="email" class="input-group-text">Name:</label><br>
+                                </div>
+                                <input type="text" id="name" name="name" placeholder="Name" required><br>
+                            </div>
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <label for="email" class="input-group-text">Email:</label><br>
+                                </div>
+                                <input type="password" class="form-control" placeholder="E-mail">
+                            </div>
+                            <div class="form-group" text="center">
+						        <input type="submit" value="Submit" class="btn float-right login_btn">
+					        </div>
+                        </form>
+    </div>
+    <div class="col">
+    <form>
+    <h2>Task-01</h2>
+                        <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <label for="email" class="input-group-text">Name:</label><br>
+                                </div>
+                                <input type="text" id="name" name="name" placeholder="Name" required><br>
+                            </div>
+                            <div class="input-group form-group">
+                                <div class="input-group-prepend">
+                                    <label for="email" class="input-group-text">Email:</label><br>
+                                </div>
+                                <input type="password" class="form-control" placeholder="E-mail">
+                            </div>
+                            <div class="form-group" text="center">
+						        <input type="submit" value="Submit" class="btn float-right login_btn">
+					        </div>
+                        </form>
+    </div>
+  </div>
+</div>
+    </body>
+</html>
+
+
+                                
