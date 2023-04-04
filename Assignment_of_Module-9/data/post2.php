6a3c43ab1267b0bf306a8ec365c873ef7ae9a2f9
H A S A N <hasan25042019@gmail.com>
2023-04-04 19:20:36 +0600

    Create post2.php
    dfd

---
+++ Assignment_of_Module-9/data/post2.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com -->
+  <title>TREE</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <style>
+
+  h2 {
+    font-size: 24px;
+    text-transform: uppercase;
+    color: #f4511e;
+    font-weight: 600;
+    margin-bottom: 3px;
+  }
+
+  .jumbotron {
+    background-color: #f4511e;
+    color: #fff;
+    padding: 100px 25px;
+    font-family: Montserrat, sans-serif;
+  }
+  .container-fluid {
+    padding: 60px 50px;
+  }
+  .bg-grey {
+    background-color: #f6f6f6;
+  }
+  .logo-small {
+    color: #f4511e;
+    font-size: 50px;
+  }
+  .logo {
+    color: #f4511e;
+    font-size: 200px;
+  }
+
+
+  .panel-heading {
+    color: #fff !important;
+    background-color: #f4511e !important;
+    padding: 25px;
+    border-bottom: 1px solid transparent;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+  }
+
+
+
+  .navbar {
+    margin-bottom: 0;
+    background-color: #f4511e;
+    z-index: 9999;
+    border: 0;
+    font-size: 12px !important;
+    line-height: 1.42857143 !important;
+    letter-spacing: 4px;
+    border-radius: 0;
+    font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+    color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+    color: #f4511e !important;
+    background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+    border-color: transparent;
+    color: #fff !important;
+  }
+
+  
+  </style>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="../index.php">BLOG</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="../index.php">HOME</a></li>
+        <li><a href="../blog.php">BLOG</a></li>
+        <li><a href="../contact.php">CONTACT</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">PORTFOLIO</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<br>
+<br>
+
+
+ <div class="container-fluid bg-grey">
+  <div class="row">
+      <div class="col-sm-8">
+        <h2>Beautiful Tree</h2>
+        <p><strong>Writer: H A S A N</strong></p>
+        <img src="https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_960_720.jpg" alt="Featured Post Image">
+        <h3>
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+       quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+       natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+       quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+       unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident 
+       placeat quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque 
+       dicta nisi unde natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum 
+       provident placeat quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a 
+       itaque dicta nisi unde natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
+       laborum provident placeat quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt
+       hic nemo a itaque dicta nisi unde natus
+     </h3>
+    </div>
+  </div>
+    
+
+         
+
+    <?php include_once("../footer.php") ?>
+</body>
+</html>
